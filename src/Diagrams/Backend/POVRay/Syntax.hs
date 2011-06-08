53b3f36870a9590e4a5fb3514c324a76531edab5
Brent Yorgey <byorgey@cis.upenn.edu>
2011-06-08 05:24:54 +0000

    clean up and comment POVRay.Syntax source

--- src/Diagrams/Backend/POVRay/Syntax.hs
+++ src/Diagrams/Backend/POVRay/Syntax.hs
@@ -6,41 +6,83 @@
 -- License     :  BSD-style (see LICENSE)
 -- Maintainer  :  diagrams-discuss@googlegroups.com
 --
--- (Partial) AST for POV-Ray syntax.
+-- A (very partial) AST for POV-Ray syntax, useful for building up an
+-- abstract POVRay scene and then serializing it to a POVRay scene
+-- description file.  Parser not included.
 --
 -----------------------------------------------------------------------------
-module Diagrams.Backend.POVRay.Syntax
-
-  (
-
-  ) where
+module Diagrams.Backend.POVRay.Syntax where
 
 import Text.PrettyPrint.HughesPJ
 
-class Pretty p where
-  ppr :: p -> Doc
+------------------------------------------------------------
+-- Pretty-printing
+------------------------------------------------------------
 
+-- | Type class for things that can be pretty-printed as POVRay source
+--   code.
+class SDL p where
+  toSDL :: p -> Doc
+
+-- | Helper function to generate a labeled block like
+--
+--   > label {
+--   >   item1
+--   >   item2
+--   >   ...
+--   > }
+--
 block :: String -> [Doc] -> Doc
 block label items = text label <+> lbrace $$ nest 4 (vcat items) $$ rbrace
 
-data SceneItem = {- SIDirective Directive -}
-                 SICamera [CameraItem]
+instance SDL String where
+  toSDL = text
+
+instance SDL Double where
+  toSDL = double
+
+instance SDL () where
+  toSDL _ = empty
+
+------------------------------------------------------------
+-- Basics
+------------------------------------------------------------
+
+type Identifier = String
+
+data Vector = VecLit Double Double Double
+
+instance SDL Vector where
+  toSDL (VecLit x y z) = text "<" <> hsep (punctuate comma (map toSDL [x,y,z])) <> text ">"
+
+data Color = RGB Vector
+
+instance SDL Color where
+  toSDL (RGB v) = text "rgb" <+> toSDL v
+
+------------------------------------------------------------
+-- Scene items
+------------------------------------------------------------
+
+-- | Top-level items that can occur in a scene.
+data SceneItem = SICamera [CameraItem]
                | SIObject Object
-               {- | SIAtmosphere Atmosphere -}
-               {- | SIGlobalSettings [GlobalItem] -}
 
-instance Pretty SceneItem where
-  ppr (SICamera cItems) = block "camera" (map ppr cItems)
-  ppr (SIObject obj)    = ppr obj
+instance SDL SceneItem where
+  toSDL (SICamera cItems) = block "camera" (map toSDL cItems)
+  toSDL (SIObject obj)    = toSDL obj
 
-data CameraItem = {- CIType CameraType -}
-                  CIVector CameraVector
+------------------------------------------------------------
+-- Camera
+------------------------------------------------------------
+
+data CameraItem = CIVector CameraVector
                 | CIModifier CameraModifier
-                {- | CIId Identifier -}
 
-instance Pretty CameraItem where
-  ppr (CIVector cv)   = ppr cv
-  ppr (CIModifier cm) = ppr cm
+instance SDL CameraItem where
+  toSDL (CIVector cv)   = toSDL cv
+  toSDL (CIModifier cm) = toSDL cm
+
 
 data CameraVector = CVLocation  Vector
                   | CVRight     Vector
@@ -48,88 +90,57 @@ data CameraVector = CVLocation  Vector
                   | CVDirection Vector
                   | CVSky       Vector
 
-instance Pretty CameraVector where
-  ppr (CVLocation v)  = text "location"  <+> ppr v
-  ppr (CVRight v)     = text "right"     <+> ppr v
-  ppr (CVUp v)        = text "up"        <+> ppr v
-  ppr (CVDirection v) = text "direction" <+> ppr v
-  ppr (CVSky v)       = text "sky"       <+> ppr v
+instance SDL CameraVector where
+  toSDL (CVLocation v)  = text "location"  <+> toSDL v
+  toSDL (CVRight v)     = text "right"     <+> toSDL v
+  toSDL (CVUp v)        = text "up"        <+> toSDL v
+  toSDL (CVDirection v) = text "direction" <+> toSDL v
+  toSDL (CVSky v)       = text "sky"       <+> toSDL v
 
-data CameraModifier = {- CMAngle Double ?? -}
-                      CMLookAt Vector
-                    {- | CMBlurSamples Int
-                       | CMAperture Double
-                       | CMFocalPoint Vector
-                       | CMConfidence Double
-                       | CMVariance Double
-                       | CMNormal Normal
-                       | CMTransform Transformation
-                    -}
+data CameraModifier = CMLookAt Vector
 
-instance Pretty CameraModifier where
-  ppr (CMLookAt v) = text "look_at" <+> ppr v
+instance SDL CameraModifier where
+  toSDL (CMLookAt v) = text "look_at" <+> toSDL v
 
-type Identifier = String
-
-instance Pretty String where
-  ppr = text
-
-data Vector = VecLit Double Double Double
-
-instance Pretty Vector where
-  ppr (VecLit x y z) = text "<" <> hsep (punctuate comma (map ppr [x,y,z])) <> text ">"
-
-instance Pretty Double where
-  ppr = double
+------------------------------------------------------------
+-- Objects
+------------------------------------------------------------
 
 data Object = OFiniteSolid FiniteSolid
-  --          | OFinitePatch FinitePatch
-  --          | OInfiniteSolid InfiniteSolid
-  --          | OIsosurface Isosurface
-  --          | OParametric Parametric
-  --          | OCSG CSG
             | OLight LightSource
-  --          | Object Identifier [ObjectModifier]
 
-instance Pretty Object where
-  ppr (OFiniteSolid fs) = ppr fs
-  ppr (OLight l)        = ppr l
-
-data FiniteSolid = Sphere Vector Double [ObjectModifier]
-  --  XXX other stuff
-
-instance Pretty FiniteSolid where
-  ppr (Sphere c r mods) = block "sphere" (cr : map ppr mods)
-    where cr = ppr c <> comma <+> ppr r
+instance SDL Object where
+  toSDL (OFiniteSolid fs) = toSDL fs
+  toSDL (OLight l)        = toSDL l
 
 data ObjectModifier = OMPigment Pigment
-  --                  | OMTransform Transformation
-  -- XXX lots of other stuff!
 
-instance Pretty ObjectModifier where
-  ppr (OMPigment p) = ppr p
+instance SDL ObjectModifier where
+  toSDL (OMPigment p) = toSDL p
 
 data Pigment = PColor Color
-  -- XXX other stuff
 
-instance Pretty Pigment where
-  ppr (PColor c) = block "pigment" [ppr c]
+instance SDL Pigment where
+  toSDL (PColor c) = block "pigment" [toSDL c]
+
+------------------------------------------------------------
+-- Finite solids
+------------------------------------------------------------
+
+data FiniteSolid = Sphere Vector Double [ObjectModifier]
+
+instance SDL FiniteSolid where
+  toSDL (Sphere c r mods) = block "sphere" (cr : map toSDL mods)
+    where cr = toSDL c <> comma <+> toSDL r
+
+------------------------------------------------------------
+-- Light sources
+------------------------------------------------------------
 
 data LightSource = LightSource Vector Color [LightModifier]
 
-instance Pretty LightSource where
-  ppr (LightSource loc c mods) = block "light_source" (lc : map ppr mods)
-    where lc = ppr loc <> comma <+> ppr c
+instance SDL LightSource where
+  toSDL (LightSource loc c mods) = block "light_source" (lc : map toSDL mods)
+    where lc = toSDL loc <> comma <+> toSDL c
 
-data Color = RGB Vector
-  --         | RBGF Vector4
-  --         | RGBT Vector4
-  --         | RGBFT Vector5
-
-instance Pretty Color where
-  ppr (RGB v) = text "rgb" <+> ppr v
-
-type LightModifier = ()  -- XXX
-
-instance Pretty () where
-  ppr _ = empty
+type LightModifier = ()
